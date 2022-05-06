1cd4353019b5a24df22296ff3b560f9044f01889
Diomar <diomarpr@gmail.com>
2022-05-05 23:14:54 -0300

    configuração básica de controle dos elementos

---
+++ etapas.js
@@ -0,0 +1,53 @@
+let estapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Vereador1',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'img1.png.', legenda: 'Vereador'}
+                ]
+            },
+
+            {
+                numero: 77222,
+                nome: 'Vereador2',
+                partido: 'DEF',
+                fotos: [
+                    {url: 'img1.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Prefeito1',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: 'img2.png', legenda: 'Prefeito'},
+                    { url: 'img2.png', legenda: 'Vice - Prefeito', small: true},
+                ]
+            },
+
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Azul',
+                fotos: [
+                    {url: 'img2.png', legenda: 'Prefeito'},
+                    {url: 'img2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
